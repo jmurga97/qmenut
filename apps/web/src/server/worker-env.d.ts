dc5abb20d4c7ab695af8e5412397c92f69b495c2
Juan Murga <juanmurga97@gmail.com>
2026-08-22 22:02:47 +0200

    update envs

--- apps/web/src/server/worker-env.d.ts
+++ apps/web/src/server/worker-env.d.ts
@@ -2,6 +2,7 @@ declare namespace Cloudflare {
   /** Bindings available to the public-menu Worker. */
   interface Env {
     API_WORKER?: Fetcher;
+    DISABLE_EDGE_CACHE?: string;
     SENTRY_DSN?: string;
     TENANT_THEME?: KVNamespace;
   }
